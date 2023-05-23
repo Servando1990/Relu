77a8dc717a1021c28bfc3c72ba9711180ead3e56
Servando <vandotorresg@gmail.com>
2023-05-23 15:29:23 +0200

    Tested date features and added conda yml

--- src/data/feature_engineering.py
+++ src/data/feature_engineering.py
@@ -1,5 +1,8 @@
 import pandas as pd
 from typing import List
+import holidays
+
+
 
 
 class FeatureEngineeringProcess:
@@ -53,10 +56,26 @@ class FeatureEngineeringProcess:
             df (pd.DataFrame):
             date_feature (str): df column to be transformed
             features (List[str]): List of date features to extract from the date_feature column
+            country (str): Country code for holiday detection
 
         Returns:
-            df: Dataframe with date transformations, (month, day, year, hour, minute, secod)
+            df: Dataframe with date transformations, (month, day, year, hour, minute, second, season, holidays)
         """
+        seasons = {
+        1: "Winter",
+        2: "Winter",
+        3: "Spring",
+        4: "Spring",
+        5: "Spring",
+        6: "Summer",
+        7: "Summer",
+        8: "Summer",
+        9: "Fall",
+        10: "Fall",
+        11: "Fall",
+        12: "Winter",
+        }
+
 
         df[date_feature] = pd.to_datetime(df[date_feature])
 
@@ -78,5 +97,16 @@ class FeatureEngineeringProcess:
             df[date_feature + "_minute"] = df[date_feature].dt.minute
         if "second" in features:
             df[date_feature + "_second"] = df[date_feature].dt.second
+        if "season" in features:
+            df[date_feature + "_season"] = df[date_feature].dt.month.map(seasons)
+        if "holidays" in features:
+            print('updated ___')
+            holiday_dates = holidays.CountryHoliday('PL', years=df[date_feature].dt.year)
+            df[date_feature + "_holidays"] = df[date_feature].dt.date.map(holiday_dates).notna().astype(int)
 
         return df
+
+    
+
+    
+
