dfa033fe95518cae0fd10062f59408232f3cd333
Servando <vandotorresg@gmail.com>
2023-07-27 00:25:17 +0300

    working on quantile xgb with modal

--- src/data/data_splitter.py
+++ src/data/data_splitter.py
@@ -4,7 +4,13 @@ from datetime import datetime, timedelta
 import numpy as np
 
 class DataSplitter:
-    def __init__(self, data, target_variable, train_months, val_months=0, test_weeks=0):
+    def __init__(self,
+                  data: pd.DataFrame,
+                  target_variable: str,
+                  date_column: str ,
+                  train_months: int,
+                  val_months:int =0,
+                  test_weeks: int =0):
         if not isinstance(data, pd.DataFrame):
             raise ValueError("data should be a pandas DataFrame")
         if not 'Date' in data.columns:
@@ -16,6 +22,7 @@ class DataSplitter:
         # Store the data and settings
         self.data = data
         self.target_variable = target_variable
+        self.date_column = date_column
         self.train_months = train_months
         self.val_months = val_months
         self.test_weeks = test_weeks
@@ -26,23 +33,23 @@ class DataSplitter:
 
     def split_data(self):
         # Define date thresholds for train, validation, test split
-        max_date = self.data['Date'].max()
+        max_date = self.data[self.date_column].max()
         train_val_threshold = max_date - pd.DateOffset(months=self.train_months + self.val_months)
         val_test_threshold = max_date - pd.DateOffset(weeks=self.test_weeks)
         
         # Split the data into training, validation and testing sets
-        train_data = self.data[self.data['Date'] <= train_val_threshold]
-        val_data = self.data[(self.data['Date'] > train_val_threshold) & (self.data['Date'] <= val_test_threshold)]
-        test_data = self.data[self.data['Date'] > val_test_threshold]
+        train_data = self.data[self.data[self.date_column] <= train_val_threshold]
+        val_data = self.data[(self.data[self.date_column] > train_val_threshold) & (self.data[self.date_column] <= val_test_threshold)]
+        test_data = self.data[self.data[self.date_column] > val_test_threshold]
 
         # Return the training, validation, testing datasets
-        X_train, y_train = train_data.drop(columns=self.target_variable), train_data[self.target_variable]
-        X_val, y_val = val_data.drop(columns=self.target_variable), val_data[self.target_variable]
-        X_test, y_test = test_data.drop(columns=self.target_variable), test_data[self.target_variable]
+        X_train, y_train = train_data.drop(columns=[self.target_variable, self.date_column]), train_data[self.target_variable]
+        X_val, y_val = val_data.drop(columns=[self.target_variable, self.date_column]), val_data[self.target_variable]
+        X_test, y_test = test_data.drop(columns=[self.target_variable, self.date_column]), test_data[self.target_variable]
 
-        print(f"Training data covers from {min(train_data['Date'])} to {max(train_data['Date'])}")
-        print(f"Validation data covers from {min(val_data['Date'])} to {max(val_data['Date'])}")
-        print(f"Test data covers from {min(test_data['Date'])} to {max(test_data['Date'])}")
+        print(f"Training data covers from {min(train_data[self.date_column])} to {max(train_data[self.date_column])}")
+        print(f"Validation data covers from {min(val_data[self.date_column])} to {max(val_data[self.date_column])}")
+        print(f"Test data covers from {min(test_data[self.date_column])} to {max(test_data[self.date_column])}")
 
         return X_train, y_train, X_val, y_val, X_test, y_test
 
