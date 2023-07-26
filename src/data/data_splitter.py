322ddaf2ed21fabdf1c1ae0b79822a0b06f06d91
Servando <vandotorresg@gmail.com>
2023-07-26 20:33:16 +0300

    training code with xgboost and w&b

--- src/data/data_splitter.py
+++ src/data/data_splitter.py
@@ -1,55 +1,52 @@
 import pandas as pd
 from datetime import datetime, timedelta
 
+import numpy as np
+
 class DataSplitter:
-    def __init__(self, data, train_months, test_months=None, test_weeks=None):
+    def __init__(self, data, target_variable, train_months, val_months=0, test_weeks=0):
         if not isinstance(data, pd.DataFrame):
             raise ValueError("data should be a pandas DataFrame")
         if not 'Date' in data.columns:
             raise ValueError("DataFrame must contain a 'Date' column")
-        if not 'SKU' in data.columns:
-            raise ValueError("DataFrame must contain a 'SKU' column")
         
         # Convert 'Date' column to datetime
         data['Date'] = pd.to_datetime(data['Date'])
         
         # Store the data and settings
         self.data = data
+        self.target_variable = target_variable
         self.train_months = train_months
-        self.test_months = test_months
+        self.val_months = val_months
         self.test_weeks = test_weeks
-        
-        # Log information about the dataset
-        min_date = self.data['Date'].min()
-        max_date = self.data['Date'].max()
-        total_months = (max_date.year - min_date.year) * 12 + max_date.month - min_date.month
-        print(f"Min Date: {min_date}, Max Date: {max_date}, Total Months: {total_months}")
+
+        # Check for validity of train, validation, test split
+        if train_months <= 0 or val_months < 0 or test_weeks < 0:
+            raise ValueError("Months and weeks must be non-negative and training months must be positive.")
 
     def split_data(self):
-        # Calculate the split date
+        # Define date thresholds for train, validation, test split
         max_date = self.data['Date'].max()
-        split_date = max_date - pd.DateOffset(months=self.train_months)
-
-        # Split the data into training and testing sets
-        train_data = self.data[self.data['Date'] <= split_date].copy()
-        test_data = self.data[self.data['Date'] > split_date].copy()
-
-        # Get the set of SKUs that are present in both the training and test sets
-        common_skus = set(train_data['SKU']).intersection(set(test_data['SKU']))
-        # Filter the training and test sets to only include the common SKUs
-        train_data = train_data[train_data['SKU'].isin(common_skus)]
-        test_data = test_data[test_data['SKU'].isin(common_skus)]
-        #TODO add a warning message if there are no common SKUs or how many SKUs were removed
-
+        train_val_threshold = max_date - pd.DateOffset(months=self.train_months + self.val_months)
+        val_test_threshold = max_date - pd.DateOffset(weeks=self.test_weeks)
         
-        # Log data types info
-        print("Train data types:")
-        print(train_data.dtypes)
-        print("\nTest data types:")
-        print(test_data.dtypes)
+        # Split the data into training, validation and testing sets
+        train_data = self.data[self.data['Date'] <= train_val_threshold]
+        val_data = self.data[(self.data['Date'] > train_val_threshold) & (self.data['Date'] <= val_test_threshold)]
+        test_data = self.data[self.data['Date'] > val_test_threshold]
 
-        # Return the training and testing datasets
-        return train_data, test_data
+        # Return the training, validation, testing datasets
+        X_train, y_train = train_data.drop(columns=self.target_variable), train_data[self.target_variable]
+        X_val, y_val = val_data.drop(columns=self.target_variable), val_data[self.target_variable]
+        X_test, y_test = test_data.drop(columns=self.target_variable), test_data[self.target_variable]
+
+        print(f"Training data covers from {min(train_data['Date'])} to {max(train_data['Date'])}")
+        print(f"Validation data covers from {min(val_data['Date'])} to {max(val_data['Date'])}")
+        print(f"Test data covers from {min(test_data['Date'])} to {max(test_data['Date'])}")
+
+        return X_train, y_train, X_val, y_val, X_test, y_test
+
+
     
 
 
