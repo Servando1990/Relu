48f5ed82b14f3cbc1843798bbaadd4557da293f8
Servando <vandotorresg@gmail.com>
2023-08-07 19:10:45 +0200

    optuna and mlflow not working between the interplay of trials quantiles and runs

--- src/models/lgb_model.py
+++ src/models/lgb_model.py
@@ -17,25 +17,36 @@ class QuantileLGBM:
         mlflow.set_experiment(self._experiment_name)
 
     def fit(self, X, y, X_val, y_val, params):
-        with mlflow.start_run() as run:
+
             for q in self._quantiles:
-                print(f'Fitting model for quantile {q}')
-                model = LGBMRegressor(objective='quantile', alpha=q,
-                                      **params,
-                                      early_stopping_rounds=self._early_stopping_rounds)
-                model.fit(X, y,
-                          eval_set=[(X_val, y_val)],
-                          verbose=10)
-                self._models[q] = model
+                with mlflow.start_run():
+                    print(f'Fitting model for quantile {q}')
+                    model = LGBMRegressor(objective='quantile', alpha=q,
+                                            **params,
+                                            early_stopping_rounds=self._early_stopping_rounds)
+                    model.fit(X, y,
+                                eval_set=[(X_val, y_val)],
+                                verbose=10)
+                    self._models[q] = model
 
-                # Log metrics and feature importances with MLflow
-                mlflow.log_params(params)
-                mlflow.log_metric(f'quantile_{q}_feature_importances', model.feature_importances_)
+                    # Log metrics and feature importances with MLflow
+                    mlflow.log_params(params)
+                    for idx, importance in enumerate(model.feature_importances_):
+                        mlflow.log_metric(f'quantile_{q}_feature_importance_{idx}', importance)
 
-    def predict(self, X):
+
+
+    def predict(self, X_val, y_val):
         preds = []
         for q in self._quantiles:
-            preds.append(self._models[q].predict(X))
+
+            pred = self._models[q].predict(X_val)
+            preds.append(pred)
+
+            #Log individual loss for this quantile
+            val_loss_q = np.mean(np.abs(y_val - pred))
+            mlflow.log_metric(f'val_loss_quantile_{q}', val_loss_q)
+
         return np.vstack(preds).T
 
     def evaluate(self, y_true, y_pred):
@@ -43,8 +54,11 @@ class QuantileLGBM:
         return np.mean(np.abs(y_true_repeated - y_pred), axis=0)
 
     def optimize(self, objective, n_trials=100):
+
+        print(f'Optimizing hyperparameters')    
         study = optuna.create_study(direction="minimize")
         study.optimize(objective, n_trials=n_trials)
+        
         return study.best_params
 
     def register_model(self, model_name, model_version):
